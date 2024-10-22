b9cc20a252dcbcd598f005020926c1ea51d07f89
Lerish-vZ <lerishavz@gmail.com>
2024-10-22 15:04:17 +0200

    update

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
         navigate("/login");
       }
       const { data } = await axios.post(
-        "http://localhost:4000",
+        "https://web-dev-authentication-project-server.onrender.com",
         {},
         { withCredentials: true }
       );
