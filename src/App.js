14420943c65824d4302f7e14b3e1764d83f7f6f9
Lerish-vZ <lerishavz@gmail.com>
2024-10-22 09:15:00 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
-import { Login, Signup } from "./pages";
-import Home from "./pages/Home";
+import { Login, Signup } from "../pages";
+import Home from "../pages/Home";
 
 function App() {
   return (
